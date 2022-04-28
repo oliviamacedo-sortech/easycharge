f0b4f19347caaaee14ed4fe91faff3269b7aa40f
oliviamacedo-sortech <olivia.macedo@sortech.com.br>
2022-04-27 23:01:57 -0300

    Criação mapper e ajuste dto

--- src/main/java/br/com/alura/srtch/teste/Main.java
+++ src/main/java/br/com/alura/srtch/teste/Main.java
@@ -1,23 +1,25 @@
 package br.com.alura.srtch.teste;
 
+import br.com.alura.srtch.dao.ClienteDao;
+import br.com.alura.srtch.dao.EnderecoDao;
 import br.com.alura.srtch.dto.ClienteDTO;
-import br.com.alura.srtch.dto.ObjetoCliente;
+import br.com.alura.srtch.mapper.ClienteMapper;
 import br.com.alura.srtch.model.Cliente;
-import br.com.alura.srtch.service.ClientesPorEstado;
+import br.com.alura.srtch.model.Endereco;
 import br.com.alura.srtch.model.StatusCliente;
+import br.com.alura.srtch.service.ClientesPorEstado;
 import br.com.alura.srtch.service.ClientesSuspensos;
+import br.com.alura.srtch.util.JPAUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.apache.commons.collections.functors.ChainedClosure;
 
+import javax.persistence.EntityManager;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -35,10 +37,10 @@ public class Main {
     if (arquivo.endsWith(".csv")) {
       try {
         Reader reader = new FileReader(arquivo);
-        CsvToBean<Cliente> csvToBean = new CsvToBeanBuilder<Cliente>(reader)
-            .withType(Cliente.class)
+        CsvToBean<ClienteDTO> csvToBean = new CsvToBeanBuilder<ClienteDTO>(reader)
+            .withType(ClienteDTO.class)
             .build();
-        clientes = csvToBean.parse();
+        clientes = ClienteMapper.passarParaCliente(csvToBean.parse());
       } catch (IOException ex) {
         throw new IllegalStateException(ex);
       }
@@ -47,8 +49,7 @@ public class Main {
         Reader reader = new FileReader(arquivo);
         ObjectMapper mapper = new ObjectMapper();
 
-        clientes = mapper.readValue(reader, new TypeReference<>() {
-        });
+        clientes = ClienteMapper.passarParaCliente(mapper.readValue(reader, new TypeReference<>() {}));
       } catch (IOException ex) {
         throw new IllegalStateException(ex);
       }
@@ -75,7 +76,28 @@ public class Main {
       System.out.printf("- o estado %s tem %d cliente(s) cadastrado(s).\n", estado, clientesDoEstado.size());
     }
 
+    testesBancos(clientes);
 
+  }
+
+  private static void testesBancos(List<Cliente> clientes) {
+    EntityManager em = JPAUtil.getEntityManager();
+
+    ClienteDao clienteDao = new ClienteDao(em);
+    EnderecoDao enderecoDao = new EnderecoDao(em);
+
+    em.getTransaction().begin();
+    for (Cliente cliente : clientes) {
+      clienteDao.cadastrar(cliente);
+    }
+
+    em.getTransaction().commit();
+
+    em.getTransaction().begin();
+    clienteDao.buscarTodos();
+
+//    em.getTransaction().commit();
+    em.close();
   }
 
 }
