cfa393015782df3b726cbcdfae18c6e5e4aa09af
oliviamacedo-sortech <olivia.macedo@sortech.com.br>
2022-05-03 10:50:29 -0300

    Conexão com o banco e listagem de cliente

--- src/main/java/com/alura/easycharge/controller/ClienteController.java
+++ src/main/java/com/alura/easycharge/controller/ClienteController.java
@@ -1,33 +1,26 @@
 package com.alura.easycharge.controller;
 
 import com.alura.easycharge.models.Cliente;
-import com.alura.easycharge.models.Endereco;
-import com.alura.easycharge.models.StatusCliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Controller
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/listaCliente")
     public String lista(Model model){
-        Cliente cliente = new Cliente();
-        cliente.setNome("Olívia");
-        cliente.setCpf("03184520145");
-        cliente.setEmail("olivia@gmail.com");
-        cliente.setProfissao("Desenvolvedor");
-        cliente.setRenda(new BigDecimal(1000));
-        cliente.setTelefone("998219292");
-        cliente.setStatus(StatusCliente.ATIVO);
-        cliente.setEndereco(new Endereco("QI 09", "9", "Perto da pracinha","Guara","Brasilia","DF"));
 
-        List<Cliente> clientes = Arrays.asList(cliente);
+        Query query = entityManager.createQuery("select c from Cliente c", Cliente.class);
+        List<Cliente> clientes = query.getResultList();
 
         model.addAttribute("clientes", clientes);
         return "listaCliente";
