48196f69f87fece8c5e30924e5233cc24d600817
oliviamacedo-sortech <olivia.macedo@sortech.com.br>
2022-06-01 11:40:00 -0300

    Iniciando validação com javascript

--- src/main/resources/static/dist/js/validacao.js
+++ src/main/resources/static/dist/js/validacao.js
@@ -1,23 +1,57 @@
-const formularioCliente = document.querySelector('.formulario-cliente');
-formularioCliente.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const inputsObrigatorios = document.querySelectorAll('input[required]');
-    inputsObrigatorios.forEach((inputObrigatorio) => {
-        if (!inputObrigatorio.value) {
-            inputObrigatorio.classList.add('is-invalid');
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid){
+        input.classList.remove('is-invalid')
+        input.classList.add('is-valid')
+        input.parentElement.querySelector('.invalid-feedback').innerHTML = ''
+    }else {
+        input.classList.add('is-invalid')
+        input.classList.remove('is-valid')
+        input.parentElement.querySelector('.invalid-feedback').innerHTML = mostraMensagemDeError(tipoInput,input)
+    }
+}
+
+const validadores = {
+    cpf:input => validaCpf(input)
+}
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(checaCPFRepetido(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf){
+            cpfValido = false
         }
-    });
-});
-const inputsObrigatorios = document.querySelectorAll('input[required]');
-inputsObrigatorios.forEach((inputObrigatorio) => {
-    inputObrigatorio.addEventListener('blur', function () {
-        if (!inputObrigatorio.value) {
-            inputObrigatorio.classList.add('is-invalid');
-            inputObrigatorio.classList.remove('is-valid');
-        }
-        else {
-            inputObrigatorio.classList.remove('is-invalid');
-            inputObrigatorio.classList.add('is-valid');
-        }
-    });
-});
+    })
+
+    return cpfValido
+};
